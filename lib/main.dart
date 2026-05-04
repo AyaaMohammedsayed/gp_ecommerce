8fea513ada9ea2254542d1b775a0419db6686e5b
menna-elbanna <mennaelbanna28@gmail.com>
2026-05-04 04:56:30 +0300

    switched main pipeline to cart (for testing)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gp_ecommerce/features/Cart/view/screens/cart_screen.dart';
 import 'core/app_theme.dart';
 import 'features/home/cubit/home_cubit.dart';
 import 'features/home/home_screen.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
             title: 'E-Commerce App',
             debugShowCheckedModeBanner: false,
             theme: AppTheme.lightTheme,
-            home: const HomeScreen(),
+            home: const CartScreen(),
           ),
         );
       },
