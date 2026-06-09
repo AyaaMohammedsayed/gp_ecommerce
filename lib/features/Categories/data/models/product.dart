5bae1b7db32436ee2613b1985aefef1b9f2029ce
Aya <ayahame99@gmail.com>
2026-06-09 11:29:13 +0300

    product model file

---
+++ lib/features/Categories/data/models/product.dart
@@ -0,0 +1,10 @@
+import 'package:gp_ecommerce/core/constants/app_images.dart';
+
+class Product {
+  String ImageName;
+  String name;
+  String price;
+  Product({required this.ImageName, required this.name, required this.price});
+}
+
+
