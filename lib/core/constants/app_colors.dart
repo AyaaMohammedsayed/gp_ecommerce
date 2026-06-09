5b1859dd58b490d2dfbad4f8c0366828b4d645a7
Aya <ayahame99@gmail.com>
2026-06-09 11:25:18 +0300

    core files for images and colors and theme

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -15,19 +15,17 @@ class AppColors {
   static const Color drawerCard = Color(0xFF1E293B);
   static const Color logoutRed = Color(0xFFB00020);
   static const Color logo = Color(0xFFB9C7FF);
-}
-
   // Dark Mode
   static const Color darkbackground = Color(0xFF0d1325);
-  static const Color secondaryColor = Color(0xFF31394D);  // appbar
-  static const Color text1 = Color(0xFFB6C4FF);           // e.g. KINETIC
-  static const Color text2 = Color(0xFFDAE2FD);           // e.g. Your Gallery
-  static const Color text3 = Color(0xFF8D90A2);           // e.g. Objects Selected
-  static const Color titleColor = Color(0xFFC3C5D9);      // item headings
+  static const Color secondaryColor = Color(0xFF31394D); // appbar
+  static const Color text1 = Color(0xFFB6C4FF); // e.g. KINETIC
+  static const Color text2 = Color(0xFFDAE2FD); // e.g. Your Gallery
+  static const Color text3 = Color(0xFF8D90A2); // e.g. Objects Selected
+  static const Color titleColor = Color(0xFFC3C5D9); // item headings
   static const Color cardColor = Color(0xFF151B2D);
   static const Color card2Color = Color(0xFF242a3b);
 
-  // cart / payment widgets 
+  // cart / payment widgets
   static const Color accentBlue = primary;
   static const Color quantityBg = surfaceVariant;
   static const Color badgeColor = surfaceVariant;
--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,11 +1,11 @@
 class AppImages {
   ///We Will Replace this file and add our Images
-  // static const String passwordIcon = 'assets/images/icons/password.svg';
-  // static const String emailIcon = 'assets/images/icons/email.svg';
-  // static const String appIcon = 'assets/images/icons/logo.svg';
-  // static const String settingsSelected =
-  //     'assets/images/icons/setting_icon_selected.svg';
-  // static const String settingsUnSelected =
-  //     'assets/images/icons/setting_icon_unselected.svg';
+  static const String ICs = 'assets/category/ICs.png';
+  static const String leds = 'assets/category/leds.png';
+  static const String sensors = 'assets/category/sensors.png';
+  static const String transformers =
+      'assets/category/Transformers.png';
+  static const String transistors =
+      'assets/category/transistors.png';
 
 }
