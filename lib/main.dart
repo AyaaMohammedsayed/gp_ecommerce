40eadad22724a379067ced92f314de1a1a289332
Aya <ayahame99@gmail.com>
2026-06-09 11:24:52 +0300

    paths added to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gp_ecommerce/features/Cart/view/screens/cart_screen.dart';
+import 'package:gp_ecommerce/features/Categories/view/screens/category_detials_screen.dart';
+import 'package:gp_ecommerce/features/Categories/view/screens/category_screen.dart';
 import 'package:gp_ecommerce/features/Home/view/screens/home_screen.dart';
-import 'package:gp_ecommerce/features/Home/view_model/home_cubit.dart';
-import 'core/app_theme.dart';
-import 'features/onboarding_screen/onboarding_screen.dart';
-import 'features/splash_screen/splash_screen.dart';
+import 'package:gp_ecommerce/features/payment/view/screens/payment_screen.dart';
+import 'package:gp_ecommerce/features/product_details/view/screens/product_details.dart';
+
+import 'core/constants/app_theme.dart';
 import 'features/Auth/view/screens/auth_screen.dart';
 import 'features/Auth/view/screens/forgot_password_screen.dart';
 import 'features/Auth/view/screens/register_screen.dart';
+import 'features/onboarding_screen/onboarding_screen.dart';
+import 'features/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +32,21 @@ class MyApp extends StatelessWidget {
           title: 'E-Commerce App',
           debugShowCheckedModeBanner: false,
           theme: AppTheme.lightTheme,
-          initialRoute: SplashScreen.routeName,
+          initialRoute: CategoriesScreen.routeName,
           routes: {
             SplashScreen.routeName: (context) => const SplashScreen(),
             OnboardingScreen.routeName: (context) => const OnboardingScreen(),
             AuthScreen.routeName: (context) => const AuthScreen(),
             RegisterScreen.routeName: (context) => const RegisterScreen(),
-            ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
+            CartScreen.routeName: (context) => const CartScreen(),
+            ForgotPasswordScreen.routeName: (context) =>
+                const ForgotPasswordScreen(),
+            HomeScreen.routeName: (context) => const HomeScreen(),
+            PaymentScreen.routeName: (context) => const PaymentScreen(),
+            CategoriesScreen.routeName: (context) => const CategoriesScreen(),
+            ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
+            CategoryDetialsScreen.routeName: (context) =>
+                CategoryDetialsScreen(),
           },
         );
       },
