7d43a294038b6f7a00c14c3b080f68f906252be9
Aya <ayahame99@gmail.com>
2026-06-09 11:26:30 +0300

    shared app bar widget

---
+++ lib/core/widgets/appbar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gp_ecommerce/core/constants/app_colors.dart';
+
+PreferredSizeWidget? appBar ()
+{
+      return AppBar(
+      backgroundColor: AppColors.background,
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        'KINETIC',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+          fontSize: 18.sp,
+        ),
+      ),
+      leading: Builder(
+        builder: (context) => IconButton(
+          icon: Icon(Icons.menu, color: Colors.white, size: 24.w),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.shopping_cart_outlined,
+            color: Colors.white,
+            size: 24.w,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(width: 8.w),
+      ],
+    );
+ 
+ 
+
+}
